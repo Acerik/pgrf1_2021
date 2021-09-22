a58481c51eb34f281e8a001f2c6f33a3e3c54725
Bruno Jezek <bruno.jezek@uhk.cz>
2021-09-22 13:29:48 +0200

    Add files from lesson 1

---
+++ src/Canvas.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+/**
+ * trida pro kresleni na platno: zobrazeni pixelu
+ *
+ * @author PGRF FIM UHK
+ * @version 2020
+ */
+
+public class Canvas {
+
+    private JFrame frame;
+    private JPanel panel;
+    private BufferedImage img;
+
+    public Canvas(int width, int height) {
+        frame = new JFrame();
+
+        frame.setLayout(new BorderLayout());
+        frame.setTitle("UHK FIM PGRF : " + this.getClass().getName());
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        panel = new JPanel() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                present(g);
+            }
+        };
+
+        panel.setPreferredSize(new Dimension(width, height));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public void clear() {
+        Graphics gr = img.getGraphics();
+        gr.setColor(new Color(0x2f2f2f));
+        gr.fillRect(0, 0, img.getWidth(), img.getHeight());
+    }
+
+    public void present(Graphics graphics) {
+        graphics.drawImage(img, 0, 0, null);
+    }
+
+    public void draw() {
+        clear();
+        drawline(10, 10, 100, 50);
+        //drawline(210, 20, 300, 20);
+    }
+
+    public void drawline(int x1, int y1, int x2, int y2) {
+        float k,q;
+
+        k=((float)(y2-y1)/(x2-x1))  ;
+        q=y1-(k*x1);
+        for (int x = x1; x <= x2; x++){
+            float y= (k*x)+q;
+            img.setRGB(x, (int)y, 0xffff00);
+        }
+    }
+
+    public void start() {
+        draw();
+        panel.repaint();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Canvas(800, 600).start());
+    }
+
+}
